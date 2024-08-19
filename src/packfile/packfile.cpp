9f01b5872310ea51e27bbb12106c30fd7f8e7fa8
Alivebyte <alivebyte.yt@gmail.com>
2024-08-19 11:32:44 +0300

    Actual packfile

--- src/packfile/packfile.cpp
+++ src/packfile/packfile.cpp
@@ -1,21 +1,52 @@
 #include "packfile.h"
 #include <iostream>
+//#include <fstream>
+
 
 int main(int argc, char* argv[])
 {
 	if (argc > 1)
 	{
 		olc::ResourcePack* pack = new olc::ResourcePack;
+		char strbuf[256];
+
 
 		if (pack)
 		{
-			pack->AddFile("content/sprites/background_test.png");
+			/*pack->AddFile("content/sprites/background_test.png");
 			pack->AddFile("content/sprites/player.png");
+			*/
 
-			pack->SavePack(argv[1], argv[2]);
+			FILE* packlist = fopen(argv[2],"r");
+
+			if (packlist == NULL)
+			{
+				std::cout << "Couldn't open list file!";
+				return 1;
+			}
+
+			while (fgets(strbuf, sizeof(strbuf),packlist))
+			{
+				if (strbuf[strlen(strbuf) - 1] == '\n')
+				{
+					strbuf[strlen(strbuf) - 1] = '\0';
+				}
+				std::cout << "Packing file " << strbuf << std::endl;
+
+				pack->AddFile(strbuf);
+			}
+			
+
+			fclose(packlist);
+
+			pack->SavePack(argv[1], argv[3]);
 
 			std::cout << "Content succesfully packed!" << std::endl;
 		}
+	}
+	else
+	{
+		std::cout << "Usage: packfile.exe <pack name> <pack list> <pack key>";
 	}
 
 	return 0;
